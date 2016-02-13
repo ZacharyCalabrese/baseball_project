ea6c7c9beadbe41189bdf6f98b43898cf72b28a5
Zachary Calabrese <zcalabrese22@gmail.com>
2016-02-13 15:09:56 -0500

    Connected postgresql db to django

--- baseball_project/settings.py
+++ baseball_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'baseball_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'baseball_project',
+        'USER': 'baseball_project_user',
+        'PASSWORD': '834na234nadgASGsndmo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
